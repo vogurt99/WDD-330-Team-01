fd34903a2d99b39d9037b1d32c1a9592d47c7074
vogurt99 <vogurtsjrg@gmail.com>
2025-09-12 03:43:44 +0800

    Netlify fix.

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,5 @@
-import { getParam } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
-import ProductDetails from "./ProductDetails.mjs";
+import { getParam } from "/js/utils.mjs";
+import ProductData from "/js/ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 const productID = getParam("product");
