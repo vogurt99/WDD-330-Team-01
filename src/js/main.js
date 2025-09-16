85852364acbec08363e611d69f6a108719f5a488
carsonlandvatter <landvattercarson@gmail.com>
2025-09-16 12:36:59 -0600

    Added newsletter to site

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import ProductData from './ProductData.mjs';
 import ProductList from './ProductList.mjs';
+import ThankYouDisplay from './newsletter.mjs';
 import { qs } from './utils.mjs';
 
 const dataSource = new ProductData('tents');
@@ -7,3 +8,4 @@ const listElement = qs('.product-list');
 const productList = new ProductList('tents', dataSource, listElement);
 
 productList.init();
+ThankYouDisplay();
